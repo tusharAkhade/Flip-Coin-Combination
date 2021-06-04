92641b7626311886546faa146a1dcb37e2082188
Tushar Akhade <tusharakhade10@gmail.com>
2021-06-04 16:08:10 +0530

    Determines the percentage of the doublet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,21 +1,37 @@
 read -p "How many times you want to loop through the coin: " num
 declare -A coinRes
-coinRes=( ['h']=0 ['t']=0 )
-head=0
-tail=1
+coinRes=( ['HH']=0 ['HT']=0 ['TH']=0 ['TT']=0 )
+hh=0
+ht=1
+th=2
+tt=3
 for(( i=1; i<=$num; i++ ))
 do
-	toss=$((RANDOM%2))
-	if [ $toss -eq $head ]
-	then
-		coinRes['h']=$(("${coinRes['h']}"+1))
-	else
-		coinRes['t']=$(("${coinRes['t']}"+1))
-	fi
+	toss=$((RANDOM%4))
+	case $toss in
+	$hh)
+		coinRes['HH']=$((${coinRes['HH']}+1))
+		;;
+	$ht)
+		coinRes['HT']=$((${coinRes['HT']}+1))
+		;;
+	$th)
+		coinRes['TH']=$((${coinRes['TH']}+1))
+		;;
+	*)
+		coinRes['TT']=$((${coinRes['TT']}+1))
+		;;
+	esac
 done
 
-sheadPercentage=$(((${coinRes['h']}*100)/num))
-stailPercentage=$((100-$sheadPercentage))
+hhWinPercentage=$(((${coinRes['HH']}*100)/num))
+htWinPercentage=$(((${coinRes['HT']}*100)/num))
+thWinPercentage=$(((${coinRes['TH']}*100)/num))
+ttWinPercentage=$((100-(hhWinPercentage+htWinPercentage+thWinPercentage)))
 
-echo Heads=${coinRes['h']} Tails=${coinRes['t']}
-echo Head Percentage="$sheadPercentage"% Tail Percentage="$stailPercentage"%
+dheadPercentage=$(($hhWinPercentage+($thWinPercentage+$htWinPercentage)/2))
+dtailPercentage=$((100-$dheadPercentage))
+
+echo HH=${coinRes['HH']} HT=${coinRes['HT']} TH=${coinRes['TH']} TT=${coinRes['TT']}
+echo "Doublet head Percentage=$dheadPercentage%"
+echo "Doublet tail Percentage=$((100-$dheadPercentage))%"
